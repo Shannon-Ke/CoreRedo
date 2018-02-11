0e81fc01e8baa6d9b99195581474ed233e1db8ed
Megan Koh <mkoh7@gatech.edu>
2018-02-11 00:12:13 -0500

    Update of Event Class

--- app/src/main/java/edu/gatech/core/ContentEvent.java
+++ app/src/main/java/edu/gatech/core/ContentEvent.java
@@ -11,23 +11,6 @@ import android.widget.ListAdapter;
 
 public class ContentEvent {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    Event nufonia = new Event();
-    nufonia.setName("Nufonia Must Fall");
-    Event basketball = new Event();
-    basketball.setName("Women's Basketball: GT v NCU");
-    Event ccf = new Event();
-    ccf.setName("Thursday Night Dinners");
-    Event qchat = new Event();
-    qchat.setName("Q-Chat: Men's Group");
-
-    Event[] events = [nufonia, basketball, ccf, qchat];
-    ListAdapter listAdapter = new ArrayAdapter<Event>(thus, android.R.layout.simple_expandable_list_item_1, events);
-    
 
 }
--- app/src/main/java/edu/gatech/core/Event.java
+++ app/src/main/java/edu/gatech/core/Event.java
@@ -4,33 +4,73 @@ package edu.gatech.core;
  * Created by Megan on 2/10/18.
  */
 
+import java.time.LocalTime;
+
+import com.google.android.gms.maps.model.LatLng;
+
 public class Event {
 
     private String name;
-    private String date;
-    private int startTime;
-    private int endTime;
-    private int length;
-    private String description;
+    private Attributes[] attributes;
+    private LocalTime start;
+    private LocalTime end;
     private String org;
-    private String location;
-    private int latitude;
-    private int longitude;
-    private Attributes attributes;
+    private LatLng loc;
+    private String date;
 
-    public Event() {
 
+    public void Event() {
     }
+
     public String getName(){
         return name;
     }
-
     public void setName(String newName){
         name = newName;
     }
 
+    public Attributes[] getAttributes(){
+        return attributes;
+    }
+    public void setAttributes(Attributes[] newAttributes){
+        attributes = newAttributes;
+    }
 
+    public LocalTime getStart(){
+        return start;
+    }
+    public void setStart(LocalTime newStart){
+        start = newStart;
+    }
 
+    public LocalTime getEnd(){
+        return end;
+    }
+    public void setEnd(LocalTime newEnd){
+        start = newEnd;
+    }
 
+    public String getOrg(){
+        return org;
+    }
+    public void setOrg(String newOrg){
+        org = newOrg;
+    }
+
+    public LatLng getLoc(){
+        return loc;
+    }
+
+    public void setLoc(LatLng newLoc){
+        loc = newLoc;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setDate(String newDate){
+        date = newDate;
+    }
 
 }
